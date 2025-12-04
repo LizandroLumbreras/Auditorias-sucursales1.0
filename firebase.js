8f8c7589f22b02b9aa63f9d98f24da8e93e588e0
LizandroLumbreras <lizandrodejesus0@gmail.com>
2025-12-03 19:44:30 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
+    authDomain: "inventariopv-643f1.firebaseapp.com",
+    projectId: "inventariopv-643f1",
+    storageBucket: "inventariopv-643f1.appspot.com",
+    messagingSenderId: "96242533231",
+    appId: "1:96242533231:web:56f5d3401b4cb77a52c78d"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);

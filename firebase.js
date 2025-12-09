becdc854a74ece572a5a9770110549159b1d4ae9
LizandroLumbreras <lizandrodejesus0@gmail.com>
2025-12-09 16:37:04 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,16 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js";
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
   authDomain: "inventariopv-643f1.firebaseapp.com",
   projectId: "inventariopv-643f1",
   storageBucket: "inventariopv-643f1.firebasestorage.app",
   messagingSenderId: "96242533231",
   appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
-});
+};
 
-const messaging = firebase.messaging();
+firebase.initializeApp(firebaseConfig);
 
-messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification.title, {
-      body: payload.notification.body,
-      icon: payload.notification.icon
-  });
-});
+export const db = firebase.firestore();
+export const messaging = firebase.messaging();

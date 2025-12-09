98b558fba49bf8b96911b7481d63f45873d90241
LizandroLumbreras <lizandrodejesus0@gmail.com>
2025-12-09 17:06:34 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,19 @@
-// Firebase modular
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
+// SDK COMPAT (para trabajar correctamente con el SW y FCM)
+import "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js";
+import "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js";
+import "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js";
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
   authDomain: "inventariopv-643f1.firebaseapp.com",
   projectId: "inventariopv-643f1",
   storageBucket: "inventariopv-643f1.firebasestorage.app",
   messagingSenderId: "96242533231",
   appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
-};
-
-// Initialize app
-export const app = initializeApp(firebaseConfig);
+});
 
 // Firestore
-export const db = getFirestore(app);
+export const db = firebase.firestore();
 
-// Messaging (solo frontend)
-export const messaging = getMessaging(app);
+// Messaging
+export const messaging = firebase.messaging();

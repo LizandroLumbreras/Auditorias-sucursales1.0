bd29c788e3c1c8116cff8973c749fab34b93f11f
LizandroLumbreras <lizandrodejesus0@gmail.com>
2025-12-09 17:07:46 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
   apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
   authDomain: "inventariopv-643f1.firebaseapp.com",
   projectId: "inventariopv-643f1",
-  storageBucket: "inventariopv-643f1.firebasestorage.app",
+  storageBucket: "inventariopv-643f1.appspot.com",   // ← CORREGIDO
   messagingSenderId: "96242533231",
   appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
 });
@@ -14,7 +14,7 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(payload.notification.title, {
-      body: payload.notification.body,
-      icon: payload.notification.icon
+    body: payload.notification.body,
+    icon: payload.notification.icon
   });
 });
